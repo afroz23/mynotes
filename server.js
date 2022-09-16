e87c4e70b4eb3367deedd130ef72f2cce0beba07
afroz23 <afroziiitian@gmail.com>
2022-09-16 10:26:40 +0530

    published

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ connectDB();
 
 const app = express(); // main thing
 app.use(express.json()); // to accept json data
+app.use(cors());
 app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
 
@@ -22,10 +23,10 @@ app.use("/api/users", userRoutes);
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "./frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
