d843257da9d401bb2bc5eb63c0829e1e683e6366
afroz23 <afroziiitian@gmail.com>
2024-05-01 16:41:03 +0530

    refactored code

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import express from "express";
+import cors from 'cors';
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import path from "path";
-import cors from "cors";
+import connectDB from './config/db.js';
 
 import noteRoutes from "./routes/noteRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -12,27 +11,16 @@ dotenv.config();
 
 connectDB();
 
-const app = express(); // main thing
-app.use(express.json()); // to accept json data
+const app = express();
+app.use(express.json());
 app.use(cors());
-app.use("/api/notes", noteRoutes);
-app.use("/api/users", userRoutes);
+app.use('/api/notes', noteRoutes);
+app.use('/api/users', userRoutes);
 
-// --------------------------deployment------------------------------
-const __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-// --------------------------deployment------------------------------
+app.get('/', (req, res) => {
+	res.send('API is running..');
+});
+  
 
 // Error Handling middlewares
 app.use(notFound);
